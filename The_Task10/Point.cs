3f7fea26f97b3fccae4c03065ed5a6df76504b2e
Владимир Петров <vmpetrov@edu.hse.ru>
2017-09-10 22:12:04 +0500

    Допилил слава Аллаху

---
+++ The_Task10/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Task10
+{
+    class Point
+    {
+        public int data;
+        public Point left, right;
+        public Point()
+        {
+            data = 0;
+            left = null;
+            right = null;
+        }
+        public Point(int d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+    }
+
+}
--- The_Task10/Program.cs
+++ The_Task10/Program.cs
@@ -8,8 +8,88 @@ namespace The_Task10
 {
     class Program
     {
+
+        static Random rnd = new Random();
+
+        static int GetInfo()
+        {
+            int info = rnd.Next(-100, 100);
+            return info;
+        }
+        //формирование идеально-сбалансированного дерева
+        static Point IdealTree(int size, Point p)
+        {
+
+            Point r;
+            int nl, nr;
+            if (size == 0) { p = null; return p; }
+            nl = size / 2;
+            nr = size - nl - 1;
+            int d = GetInfo();
+            r = new Point(d);
+            r.left = IdealTree(nl, r.left);
+            r.right = IdealTree(nr, r.right);
+            p = r;
+            return p;
+        }
+        //печать дерева по уровням
+        static void ShowTree(Point p, int l)
+        {
+            if (p != null)
+            {
+                ShowTree(p.left, l + 4);//переход к левому поддереву
+                for (int i = 0; i < l; i++) Console.Write(" ");
+                Console.WriteLine(p.data);
+                ShowTree(p.right, l + 4);//переход к правому поддереву
+            }
+        }
+
+        public static int num;
+        static void Tree(Point p, int l)
+        {
+            if (p != null)
+            {
+                Tree(p.left, l + 3);//переход к левому поддереву
+                for (int i = 0; i < l; i++) //Console.Write(" ");
+                //Console.WriteLine(p.data);
+                Tree(p.right, l + 3);//переход к правому поддереву
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Рисую рандомное дерево:");
+            Random rand = new Random();
+            int size = rand.Next(5, 15);
+            Point idTree = null;
+            idTree = IdealTree(size, idTree);
+            ShowTree(idTree, 1);
+            Console.WriteLine("Считаем ярусы и веточки: ");
+            int irus = 0;
+            for (int i = 1; i <= size; i *= 2)
+            {
+                irus += 1;
+            }
+            Console.WriteLine("Количество ярусов: " + irus);
+
+            int[] list = new int[irus];
+            int n = 0, sizeTemp = size;
+
+            for (int i = 1; i <= size; i *= 2)
+            {
+                list[n] = i;
+                sizeTemp -= i;
+                n++;
+            }
+            n--;
+            list[n] += sizeTemp;
+            for(int i = 0; i < irus; i++)
+            Console.WriteLine("Количество веточек на " + (i+1) + " ярусе: " + list[i]);
+            Console.ReadKey();
         }
+
+
     }
 }
